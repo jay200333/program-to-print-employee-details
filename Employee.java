abcd2e8465821f0d7e66219bcbd6334d82bdd472
jay200333 <83073900+jay200333@users.noreply.github.com>
2021-06-26 08:41:20 +0530

    printing employee details

---
+++ Employee.java
@@ -0,0 +1,19 @@
+class Employee
+
+{
+
+public static void main (String args[])
+
+{
+
+System.out.println("Name-----Year of joining----- Address-------salary");
+
+System.out.println("Robert       1994         64C- WallsStreet    12k");
+
+System.out.println("Sam          2000         68D- WallsStreet    24k");
+
+System.out.println("John         1999         26B- WallsStreet    30k");
+
+}
+
+}
